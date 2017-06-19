a37573d587bd962241b68cddd7869be5a46f38aa
epijim <james@epijim.uk>
2017-06-19 13:38:19 -0400

    fix github

--- git-activity.widget/index.js
+++ git-activity.widget/index.js
@@ -1,4 +1,4 @@
-command: `./github-activity.widget/run.sh`,
+command: `./git-activity.widget/run.sh`,
 refreshFrequency: 3600000,
 style: "\
 #github-activity {\
@@ -8,7 +8,7 @@ style: "\
   transform: translateX(-50%) translateY(-50%);\
 }",
 render: () => {
-	return "<style>#github-activity-widget-index-js { width: 100%; height: 100%; }</style>";
+	return "<style>#git-activity-widget-index-js { width: 100%; height: 100%; }</style>";
 },
 update: (output, domEl) => {
 	domEl.innerHTML += output;
